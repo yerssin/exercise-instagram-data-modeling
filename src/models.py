ff3d4a80150861619986de30915d7af0f3ba96c1
yerssin <yerssin15@gmail.com>
2021-04-22 19:12:22 +0000

    creando table base de datos

--- src/models.py
+++ src/models.py
@@ -8,26 +8,57 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+
+class Posts(Base):
+    __tablename__ = 'posts'
+    # Here we define columns for the table posts.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class History(Base):
+    __tablename__ = 'history'
+    # Here we define columns for the table history
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table media
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('posts.id'))
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table comment
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    autor_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('posts.id'))
+    comment_text = Column(String(250))
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
